cdf24287347ffcae4b9c668081c245c07d5171e0
Apfirebolt <aspper20@gmail.com>
2025-03-29 01:57:10 +0530

    upgraded packages

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let year: number = new Date().getFullYear();
+    let menuItems = [
+		{ name: 'Home', url: '/' },
+		{ name: 'About', url: '/about' },
+		{ name: 'Contact', url: '/contact' },
+	];
+</script>
+
+<footer class="bg-midnight text-white py-6 border-t border-gray-700">
+    <div class="container mx-auto text-center space-y-4 pb-8">
+        <nav class="flex justify-center space-x-6">
+            {#each menuItems as item}
+                <a href={item.url} class="hover:underline hover:underline-offset-4 mx-4 hover:scale-150 transition-transform duration-200">
+                    {item.name}
+                </a>
+            {/each}
+        </nav>
+        <p class="text-xl">
+            &copy; {year} Sveltekit Games. All rights reserved.
+        </p>
+    </div>
+</footer>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,156 @@
+<script lang="ts">
+    import { fly } from "svelte/transition";
+    import { onMount } from "svelte";
+    import { writable } from "svelte/store";
+    import { count } from "$lib/store";
+    import HeaderComponent from "$lib/components/Header.svelte";
+    import type { Category, Glass, Ingredient } from "$lib/types/Cocktail";
+  
+    let categories: Category[] = [];
+    let ingredients: Ingredient[] = [];
+    let glasses: Glass[] = [];
+    let error = null;
+  
+    let text = "Welcome to Svelte Games";
+    let displayedText = "";
+    let index = 0;
+  
+    // Typewriter effect logic
+    const typeWriter = () => {
+      if (index < text.length) {
+        displayedText += text[index];
+        index++;
+        setTimeout(typeWriter, 100); // Adjust speed here
+      }
+    };
+  
+    // Start the typewriter effect when the component is mounted
+    typeWriter();
+  
+    // Fetch data from the Cocktails API
+    async function fetchCocktailData() {
+      try {
+        const [categoriesRes, ingredientsRes, glassesRes] = await Promise.all([
+          fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"),
+          fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"),
+          fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list"),
+        ]);
+  
+        const categoriesData: { drinks: Category[] } = await categoriesRes.json();
+        const ingredientsData: { drinks: Ingredient[] } = await ingredientsRes.json();
+        const glassesData: { drinks: Glass[] } = await glassesRes.json();
+  
+        categories = categoriesData.drinks;
+        ingredients = ingredientsData.drinks;
+        glasses = glassesData.drinks;
+      } catch (err) {
+        error = "Failed to fetch data from the Cocktails API.";
+        console.error(err);
+      }
+    }
+  
+    onMount(fetchCocktailData);
+  </script>
+  
+  <svelte:head>
+    <title>Cocktails - Explore Categories, Ingredients, and Glasses</title>
+    <meta
+      name="description"
+      content="Discover cocktail categories, ingredients, and glasses with our interactive page powered by the Cocktails API."
+    />
+  </svelte:head>
+  
+  <HeaderComponent />
+  
+  <section
+    class="relative bg-cover bg-center h-[500px]"
+    style="background-image: url('https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/03/pokemon-emerald-key-art-rayquaza.jpg');"
+  >
+    <div
+      class="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black bg-opacity-50 flex items-center justify-center"
+    >
+      <div class="text-center text-white">
+        <h1
+          class="text-4xl md:text-6xl font-bold mb-4"
+          in:fly={{ y: 50, duration: 500 }}
+        >
+          {displayedText}
+        </h1>
+        <p
+          class="text-lg md:text-xl mb-6"
+          in:fly={{ y: 50, duration: 500, delay: 200 }}
+        >
+          Discover your favorite games and more
+        </p>
+        <button
+          class="px-6 py-3 bg-midnight hover:bg-blue-700 text-white font-semibold rounded-lg"
+          in:fly={{ y: 50, duration: 500, delay: 400 }}
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  </section>
+  
+  <section class="py-12 bg-gray-100">
+    <div class="container mx-auto px-6">
+      <h2
+        class="text-3xl font-bold text-center mb-8"
+        in:fly={{ y: 50, duration: 500 }}
+      >
+        Explore Our Ingredients
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {#each ingredients as ingredient, i}
+          <div
+            class="text-center p-6 bg-white shadow-lg rounded-lg"
+            in:fly={{ y: 50, duration: 500, delay: i * 100 }}
+          >
+            <p class="px-2 py-1 bg-accent text-gray-800 my-1">
+              {ingredient.strIngredient1}
+            </p>
+          </div>
+        {/each}
+      </div>
+    </div>
+  
+    <div class="container mx-auto px-6 mt-12">
+      <h2
+        class="text-3xl font-bold text-center mb-8"
+        in:fly={{ y: 50, duration: 500 }}
+      >
+        Explore Our Glasses
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {#each glasses as glass, i}
+          <div
+            class="text-center p-6 bg-white shadow-lg rounded-lg"
+            in:fly={{ y: 50, duration: 500, delay: i * 100 }}
+          >
+            <p class="px-2 py-1 bg-accent text-gray-800 my-1">{glass.strGlass}</p>
+          </div>
+        {/each}
+      </div>
+    </div>
+  
+    <div class="container mx-auto px-6 mt-12">
+      <h2
+        class="text-3xl font-bold text-center mb-8"
+        in:fly={{ y: 50, duration: 500 }}
+      >
+        Explore Our Categories
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {#each categories as category, i}
+          <div
+            class="text-center p-6 bg-white shadow-lg rounded-lg"
+            in:fly={{ y: 50, duration: 500, delay: i * 100 }}
+          >
+            <p class="px-2 py-1 bg-accent text-gray-800 my-1">
+              {category.strCategory}
+            </p>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </section>  
